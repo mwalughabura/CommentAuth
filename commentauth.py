8ed3aa6b30850f98c14c307dab564ac387de77f4
mwalughabura <mwalughabura@gmail.com>
2018-03-02 11:15:40 +0300

    Add the prompt for creating a new user

--- commentauth.py
+++ commentauth.py
@@ -1,3 +1,5 @@
+import getpass
+
 all_users=[]
 
 class User(object):
@@ -9,6 +11,7 @@ class User(object):
 
 	def create_user(self):
 		pass
+		
 
 	def create_comment(self):
 		pass
@@ -19,5 +22,15 @@ class User(object):
 	def update_comment(self):
 		pass
 
-
-
+print ("Hello weclcome to CommentAuth.")
+if len (all_users) == 0:
+	answer = input ("Do you have an account\n 1. Yes\n 2. No \n >>> ")
+	# while  answer != "Yes" or answer != "No":
+	# 	username = input ("Input either Yes or No :: The first letter is capital. \n >>> ")
+	if answer == "No":
+		username = input ("What is your username? \n >>> ")
+		password = getpass.getpass ("Input a password please \n >>> ")
+	elif answer == "Yes":
+		pass
+		
+	print ("%s %s" %(username, password))
