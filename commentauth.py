befe46ab7fd46ed1f91942777176b35d7e19c9c0
omariba <omariba5@gmail.com>
2018-03-02 10:43:45 +0300

    Add user class

---
+++ commentauth.py
@@ -0,0 +1,23 @@
+all_users=[]
+
+class User(object):
+	usertype = "regular"
+
+	def __init__(self,username,password):
+		self.username = username
+		self.password = password
+
+	def create_user(self):
+		pass
+
+	def create_comment(self):
+		pass
+
+	def view_comments(self):
+		pass
+
+	def update_comment(self):
+		pass
+
+
+
