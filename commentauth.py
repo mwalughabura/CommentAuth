fa84c5e060c5ea7bde68e16bfa7f6b9e2f4cd25b
mwalughabura <mwalughabura@gmail.com>
2018-03-02 12:09:49 +0300

    just for presentation

--- commentauth.py
+++ commentauth.py
@@ -1,6 +1,6 @@
 import getpass
 
-all_users=[]
+all_users={}
 
 class User(object):
 	usertype = "regular"
@@ -9,8 +9,8 @@ class User(object):
 		self.username = username
 		self.password = password
 
-	def create_user(self):
-		pass
+	# def create_user(self):
+	# 	pass
 		
 
 	def create_comment(self):
@@ -22,15 +22,37 @@ class User(object):
 	def update_comment(self):
 		pass
 
+more_users = {}
+
 print ("Hello weclcome to CommentAuth.")
 if len (all_users) == 0:
 	answer = input ("Do you have an account\n 1. Yes\n 2. No \n >>> ")
-	# while  answer != "Yes" or answer != "No":
-	# 	username = input ("Input either Yes or No :: The first letter is capital. \n >>> ")
-	if answer == "No":
+
+	if str(answer) == "No":
 		username = input ("What is your username? \n >>> ")
 		password = getpass.getpass ("Input a password please \n >>> ")
-	elif answer == "Yes":
+		all_users[username] = password
+		new_user = User(username, password)
+		all_users[username] = new_user
+		print ("\nSuccessfully added new user\n")
+
+	elif str(answer) == "Yes":
 		pass
-		
-	print ("%s %s" %(username, password))
+
+	else:
+		print ("Invalid respose.")
+		exit()
+
+
+comment = input ("Write a comment about this app. \n >>> ")
+more_users["first comment"] = comment
+
+print ("Now edit the comment.\n\n")
+comment = input ("Edit the comment that you wrote. \n >>> ")
+more_users["second comment"] = comment
+
+# print (all_users)
+
+print ("\nThe first comment was '%s' now the comment is '%s'\n" %(more_users["first comment"], more_users["second comment"]))
+
+# print ("Your username is: %s your password is :%s" %(username, password))
